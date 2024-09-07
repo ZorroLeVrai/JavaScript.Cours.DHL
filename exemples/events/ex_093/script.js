272af66f705f8a4983c6b26ce8107b0b08ab7716
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-09-07 10:46:45 +0200

    Ajout exemple URL

---
+++ exemples/events/ex_093/script.js
@@ -0,0 +1,10 @@
+// Exemple "https:"
+console.log('Protocole:', window.location.protocol);
+// Exemple "www.example.com"
+console.log('Nom de domaine:', window.location.host);
+// Exemple "/page1"
+console.log('Chemin:', window.location.pathname);
+// Exemple "?param=valeur"
+console.log('Paramètres:', window.location.search);
+// Exemple "#section1"
+console.log('Ancre:', window.location.hash);
