693e45f6dba2471248e437331af298d4928144d3
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-10-11 18:50:18 +0200

    Ajout des exemples pour le CSS

---
+++ exemples/CSS/ex_102/script.js
@@ -0,0 +1,3 @@
+const myDiv = document.getElementById('myDiv');
+// Accès et modification d'une propriété CSS via l'objet DOM
+myDiv.style.backgroundColor = 'lightgreen';  // Changement de la couleur de fond
---
+++ exemples/CSS/ex_103/script.js
@@ -0,0 +1,5 @@
+const myDiv = document.getElementById('myDiv2');
+// Modification d'une propriété CSS via l'objet DOM
+myDiv.style.backgroundColor = 'blue';  // Changement de la couleur de fond
+myDiv.style.width = '300px';  //Met la largeur à 300px
+myDiv.style.height = '300px';  //Met la heuteur à 300px
