478e5fc88448aa4660000e53fbe8ac16d1844e8f
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-10-27 04:56:04 +0100

    MAJ exemple 107

--- exemples/Formulaires/ex_107/script.js
+++ exemples/Formulaires/ex_107/script.js
@@ -1,6 +1,11 @@
-const firstNameElement = document.getElementById("firstname");
-const lastNameElement = document.getElementById("lastname");
+const firstNameElement = document.getElementById("prenom");
+const lastNameElement = document.getElementById("nom");
 const formElement = document.getElementById("myForm");
+const fillFormButtonElement = document.getElementById("fillFormButton");
+const initFormButtonElement = document.getElementById("initFormButton");
+
+fillFormButtonElement.addEventListener("click", fillForm);
+initFormButtonElement.addEventListener("click", initForm);
 
 function fillForm() {
   firstNameElement.value = "John";
