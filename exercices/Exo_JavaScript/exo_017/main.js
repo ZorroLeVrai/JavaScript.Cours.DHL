6df3e9e1481a5d7891160578564273389c61ffdd
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-11-22 04:10:18 +0100

    MAJ exercice 17

--- exercices/Exo_JavaScript/exo_017/main.js
+++ exercices/Exo_JavaScript/exo_017/main.js
@@ -1,4 +1,4 @@
-import { AjoutModificationStatus } from './tools.js';
+import { Task, TaskStatus, AjoutModificationStatus } from './tools.js';
 
 const ajoutTacheElement = document.getElementById('ajouter_tache');
 const modifierTacheElement = document.getElementById('modifier_tache');
@@ -18,6 +18,11 @@ const suppressionTacheElement = document.getElementById('suppression_taches');
 const validerSuppressionTacheElement = document.getElementById('valider_suppression_tache');
 const annulerSuppressionTacheElement = document.getElementById('annuler_suppression_tache');
 
+//Affichage des tâches
+const tachesAFaireElement = document.getElementById('liste_taches_a_faire');
+const tachesEnCoursElement = document.getElementById('liste_taches_en_cours');
+const tachesTermineElement = document.getElementById('liste_taches_terminees');
+
 //Abonnement aux evenements
 ajoutTacheElement.addEventListener('click', () => afficherAjoutMenu(AjoutModificationStatus.AJOUT));
 modifierTacheElement.addEventListener('click', () => afficherAjoutMenu(AjoutModificationStatus.MODIFICATION));
@@ -30,6 +35,25 @@ validerSuppressionTacheElement.addEventListener('click', validerSuppressionTache
 annulerSuppressionTacheElement.addEventListener('click', () => suppressionTacheElement.classList.add('remove'));
 
 let modeEditionMenu = AjoutModificationStatus.AJOUT;
+
+/**
+ * Retourne l'element parent pour le statut de la tache
+ * @param {string} taskStatus 
+ * @returns {HTMLElement | null} l'element parent pour le statut de la tache
+ */
+function obtenirElementParentPourStatut(taskStatus) {
+  switch (taskStatus) {
+    case TaskStatus.A_FAIRE:
+      return tachesAFaireElement;
+    case TaskStatus.EN_COURS:
+      return tachesEnCoursElement;
+    case TaskStatus.TERMINE:
+      return tachesTermineElement;
+    default:
+      return null;
+  }
+}
+
 
 /**
  * Afficher le menu d'ajout / modification de tache
@@ -58,7 +82,76 @@ function afficherSuppressionMenu() {
 function validerModificationTache() {
   console.log(modeEditionMenu);
   ajoutModificationTacheElement.classList.add("remove");
+  const tache = creerTacheDuFormulaire();
+  ajouterTache(tache);
+
   initAjoutModificationTachesMenu();
+}
+
+/**
+ * Ajoute une tache dans la bonne
+ * @param {Task} task tache a ajouter
+ */
+function ajouterTache(task) {
+  const tacheElement = creerElementTache(task);
+  const parentElement = obtenirElementParentPourStatut(task.status);
+  parentElement.appendChild(tacheElement);
+}
+
+
+/**
+ * Retourne un nouvel élément du DOM pour une tache
+ * @param {Task} task task à utiliser pour créer le nouvel élément du DOM
+ * @returns {HTMLLIElement} nouvel élément du DOM
+ */
+function creerElementTache(task) {
+  const taskIdStr = task.id.toString();
+
+  const tacheElement = document.createElement("li");
+  const checkboxElement = document.createElement("input");
+  checkboxElement.type = "checkbox";
+  checkboxElement.id = taskIdStr;
+  const cbLabelElement = document.createElement("label");
+  cbLabelElement.htmlFor = taskIdStr;
+  cbLabelElement.textContent = task.title;
+
+  tacheElement.setAttribute("data-id", task.id);
+  tacheElement.appendChild(checkboxElement);
+  tacheElement.appendChild(cbLabelElement);
+
+  return tacheElement;
+}
+
+/**
+ * Créer une tache a partir des informations du formulaire
+ * @returns {Task} La tache creee a partir des informations du formulaire
+ */
+function creerTacheDuFormulaire() {
+  const titre = nomTacheElement.value;
+  const valeurStatut = statutTacheElement.value;
+  const estUrgent = tacheUrgenteElement.checked;
+
+  const statut = tacheStatutValeurVersStatus(valeurStatut);
+
+  return new Task(titre, statut, estUrgent);
+}
+
+/**
+ * Retourner le statut de la tache
+ * @param {string} valeur valeur du statut de la tache dans le formulaire
+ * @returns {Object} le statut de la tache
+ */
+function tacheStatutValeurVersStatus(valeur) {
+  switch (valeur) {
+    case "1":
+      return TaskStatus.A_FAIRE;
+    case "2":
+      return TaskStatus.EN_COURS;
+    case "3":
+      return TaskStatus.TERMINE;
+    default:
+      return null;
+  }
 }
 
 /**
--- exercices/Exo_JavaScript/exo_017/tools.js
+++ exercices/Exo_JavaScript/exo_017/tools.js
@@ -3,3 +3,26 @@ export const AjoutModificationStatus = Object.freeze({
     MODIFICATION: "MODIFICATION"
 });
 
+export const TaskStatus = Object.freeze({
+    A_FAIRE: "A_FAIRE",
+    EN_COURS: "EN_COURS",
+    TERMINE: "TERMINE"
+});
+
+function* createIdIterator() {
+    let id = 0;
+    while (true) {
+        yield ++id;
+    }
+}
+
+const idIterator = createIdIterator();
+
+export class Task {
+    constructor(title, status, isUrgent) {
+        this.id = idIterator.next().value;
+        this.title = title;
+        this.status = status;
+        this.isUrgent = isUrgent;
+    }
+}
